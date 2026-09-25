3e694827d915b59ea7f42b4fc0bfb94af186ea67
saramohamed504 <92696069+sara55m@users.noreply.github.com>
2026-09-25 03:39:29 +0300

    trigger redeploy

--- app/Filament/Resources/Payments/Schemas/PaymentForm.php
+++ app/Filament/Resources/Payments/Schemas/PaymentForm.php
@@ -2,17 +2,8 @@
 
 namespace App\Filament\Resources\Payments\Schemas;
 
-use App\Enums\PaymentMethod;
-use App\Enums\PaymentStatus;
-use Filament\Forms\Components\DateTimePicker;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
-use Filament\Schemas\Components\Tabs;
-use Filament\Schemas\Components\Tabs\Tab;
-use App\Models\Booking;
-use Filament\Schemas\Components\Utilities\Get;
-use Filament\Forms\Components\Hidden;
+
 
 class PaymentForm
 {
@@ -20,7 +11,7 @@ class PaymentForm
     {
         return $schema
             ->components([
-                
+
             ]);
     }
 }
