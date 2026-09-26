aad133c68579055d6942adc259429f3576bc3065
saramohamed504 <92696069+sara55m@users.noreply.github.com>
2026-09-26 08:18:51 +0300

    add redis scheme to database config

--- config/database.php
+++ config/database.php
@@ -152,6 +152,7 @@ return [
         ],
 
         'default' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
@@ -161,6 +162,7 @@ return [
         ],
 
         'cache' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
